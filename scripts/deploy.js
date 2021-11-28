11282d0e87fae0c9eacbc19326c0f0a701d029ee
justinkuhn <justintkuhn@gmail.com>
2021-11-28 13:54:20 +0000

    nft game built

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -16,23 +16,7 @@ const main = async () => {
   );
   await gameContract.deployed();
    console.log("Contract deployed to:", gameContract.address);
-   
-   
-let txn;
-// We only have three characters.
-// an NFT w/ the character at index 2 of our array.
-txn = await gameContract.mintCharacterNFT(4);
-  await txn.wait();
   
-txn = await gameContract.attackBoss();
-  await txn.wait();
-
-  txn = await gameContract.attackBoss();
-await txn.wait();
-
-// Get the value of the NFT's URI.
-let returnedTokenUri = await gameContract.tokenURI(1);
-console.log("Token URI:", returnedTokenUri);
 };
 
 const runMain = async () => {
